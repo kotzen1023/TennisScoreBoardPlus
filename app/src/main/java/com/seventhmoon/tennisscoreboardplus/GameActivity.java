3fca6a319843e5badb3bf07c115676103eaaec23
richie <kotzen1023@gmail.com>
2017-01-18 17:14:34 +0800

    modify

--- app/src/main/java/com/seventhmoon/tennisscoreboardplus/GameActivity.java
+++ app/src/main/java/com/seventhmoon/tennisscoreboardplus/GameActivity.java
@@ -165,13 +165,27 @@ public class GameActivity extends AppCompatActivity{
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    private Sensor mGravity;
+    private Sensor mGyroscope;
+    private Sensor mGyroscope_uncalibrated;
+    private Sensor mLinearAcceration;
+    private Sensor mRotationVector;
+    private Sensor mStepCounter;
+
+
     private SensorEventListener accelerometerListener;
 
     private static double PI = 3.1415926535897932384626433832795;
-    private static double gravity = 9.80665;
+    private static double gravity = 9806.65;
 
     private static long previous_time = 0;
     private static long current_time = 0;
+    private static double previous_accel = 0.0;
+    private static double current_accel = 0.0;
+
+    //private static double x_coordinate = 0.0;
+    //private static double y_coordinate = 0.0;
+    //private static double z_coordinate = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -194,25 +208,75 @@ public class GameActivity extends AppCompatActivity{
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
         if (mAccelerometer != null) {
             Log.e(TAG, "Has mAccelerometer sensor!");
         }
+
+        mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        if (mGravity != null) {
+            Log.e(TAG, "Has gravity sensor!");
+        }
+
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (mGyroscope != null) {
+            Log.e(TAG, "Has gyroscope sensor!");
+        }
+
+        mGyroscope_uncalibrated = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        if (mGyroscope_uncalibrated != null) {
+            Log.e(TAG, "Has gyroscope uncalibrate sensor!");
+        }
+
+        mLinearAcceration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        if (mLinearAcceration != null) {
+            Log.e(TAG, "Has linear acceleration sensor!");
+        }
+
+        mRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        if (mRotationVector != null) {
+            Log.e(TAG, "Has rotation vector sensor!");
+        }
+
+        mStepCounter = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (mStepCounter != null) {
+            Log.e(TAG, "Has step counter sensor!");
+        }
+
+
 
         accelerometerListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 current_time = System.currentTimeMillis();
-                double accel = sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]);
+                current_accel = sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
                 //Log.d(TAG, "accel = "+accel+" sec = "+(current_time-previous_time));
                 //Log.d(TAG, "System.currentTimeMillis() = "+System.currentTimeMillis());
                 //Log.d(TAG, "X: " + String.valueOf(event.values[0]));
                 //Log.d(TAG, "Y: " + String.valueOf(event.values[1]));
                 //Log.d(TAG, "Z: " + String.valueOf(event.values[2]));
-                double time = (double) ((current_time-previous_time)/1000);
-                double distance = Accel2mms(accel, time);
-                Log.d(TAG, "accel = "+accel+" sec = "+time+" distance = "+distance);
+                double time = (((double)(current_time-previous_time))/1000);
+
+                //x_coordinate = x_coordinate + (event.values[0] * time * time)*100;
+                //y_coordinate = x_coordinate + (event.values[1] * time * time)*100;
+                //z_coordinate = z_coordinate + (event.values[2] * time * time)*100;
+
+
+
+                //double distance = Accel2mms(accel, time);
+                //if (distance > 0.1)
+                double accel_diff;
+                if (current_accel > previous_accel)
+                    accel_diff = current_accel - previous_accel;
+                else
+                    accel_diff = previous_accel - current_accel;
+
+                double distance = accel_diff * time * time;
+
+                if (accel_diff > 0.2)
+                   Log.d(TAG, "accel_diff = "+accel_diff+" sec = "+time+" distance = "+distance+" move = "+distance/accel_diff);
+                //Log.d(TAG, "x = "+x_coordinate+", y = "+y_coordinate+", z = "+z_coordinate);
                 previous_time = current_time;
+                previous_accel = current_accel;
             }
 
             @Override
@@ -222,7 +286,7 @@ public class GameActivity extends AppCompatActivity{
             }
         };
 
-        mSensorManager.registerListener(accelerometerListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(accelerometerListener, mLinearAcceration, SensorManager.SENSOR_DELAY_NORMAL);
 
         //for action bar
         ActionBar actionBar = getSupportActionBar();
